d535e8494c49ac8d5bb1e2a9f7ab6b1b317e290a
imatrix04 <florent.germain040105@orange.fr>
2025-12-16 10:27:22 +0100

    post book

--- src/routes/book.routes.js
+++ src/routes/book.routes.js
@@ -12,4 +12,29 @@ router.get('/books', async (req, res) => {
   }
 });
 
+router.post('/books', async (req, res) => {
+  const { title, author, year, available, image } = req.body;
+
+  // Simple validation
+  if (!title || !author) {
+    return res.status(400).json({ error: 'Title and author are required' });
+  }
+  try {
+    const query = `
+      INSERT INTO books (title, author, year, available, image)
+      VALUES ($1, $2, $3, $4, $5)
+      RETURNING *;
+    `;
+
+    const values = [title, author, year || null, available !== undefined ? available : true, image || null];
+
+    const result = await pool.query(query, values);
+
+    res.status(201).json(result.rows[0]); // renvoie le livre créé
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Erreur serveur' });
+  }
+});
+
 module.exports = router;
